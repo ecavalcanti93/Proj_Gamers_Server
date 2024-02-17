efd34e38e9998f742db7a394c586bc88ff6309c8
Marcel Costa <marcelcosta@gmail.com>
2024-02-17 13:07:13 +0100

    modificar modelu user

--- models/User.model.js
+++ models/User.model.js
@@ -3,6 +3,14 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    username: {
+      type: String,
+      required: [true, 'Username is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+
     email: {
       type: String,
       required: [true, 'Email is required.'],
@@ -13,7 +21,13 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, 'Password is required.']
-    }
+    },
+
+    userImage: {
+      type: String,
+      default: '/images/default-avatar.jpg',
+    },
+    games: [{ type: Schema.Types.ObjectId, ref: "Games" }]
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
