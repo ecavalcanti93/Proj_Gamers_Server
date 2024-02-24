61e18cb98a58a7502cb6ad3401c09798988b2aff
Marcel Costa <marcelcosta@gmail.com>
2024-02-24 10:27:14 +0100

    done

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const commentsRoutes = require('./routes/comments.routes')
 app.use('/comments', isAuthenticated, commentsRoutes);
 
 const authRoutes = require('./routes/auth.routes')
-app.use('/auth', authRoutes);
+app.use('/', authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
